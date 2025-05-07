d7f1f41a805995bc72b0dc9a19cade2dddd2a9a4
Hugo EXTRAT <extrat.h@gmail.com>
2025-05-07 17:01:25 +0200

    feat: new impl supporting 0.45.X

--- ios/StripeSdk.mm
+++ ios/StripeSdk.mm
@@ -400,6 +400,20 @@ RCT_EXPORT_METHOD(clearEmbeddedPaymentOption:(NSInteger)viewTag
   resolve(nil);
 }
 
+RCT_EXPORT_METHOD(createPaymentMethodCustomNative:(nonnull NSDictionary *)params
+                               resolve:(nonnull RCTPromiseResolveBlock)resolve
+                                reject:(nonnull RCTPromiseRejectBlock)reject)
+{
+  [StripeSdkImpl.shared createPaymentMethodCustomNative:params resolver:resolve rejecter:reject];
+}
+
+RCT_EXPORT_METHOD(getNetworksForCard:(nonnull NSDictionary *)params
+                               resolve:(nonnull RCTPromiseResolveBlock)resolve
+                                reject:(nonnull RCTPromiseRejectBlock)reject)
+{
+  [StripeSdkImpl.shared getNetworksForCard:params resolver:resolve rejecter:reject];
+}
+
 /* clang-format on */
 
 #ifdef RCT_NEW_ARCH_ENABLED
