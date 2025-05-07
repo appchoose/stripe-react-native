5a1c6425a4a5288fb4390640069aaea5f212e0bc
Hugo EXTRAT <extrat.h@gmail.com>
2025-05-07 18:06:44 +0200

    feat: new impl supporting 0.45.X (#5)

--- android/src/oldarch/java/com/reactnativestripesdk/NativeStripeSdkModuleSpec.java
+++ android/src/oldarch/java/com/reactnativestripesdk/NativeStripeSdkModuleSpec.java
@@ -307,4 +307,12 @@ public abstract class NativeStripeSdkModuleSpec extends ReactContextBaseJavaModu
   @ReactMethod
   @DoNotStrip
   public abstract void clearEmbeddedPaymentOption(double viewTag, Promise promise);
+
+  @ReactMethod
+  @DoNotStrip
+  public abstract void createPaymentMethodCustomNative(ReadableMap params, Promise promise);
+
+  @ReactMethod
+  @DoNotStrip
+  public abstract void getNetworksForCard(ReadableMap params, Promise promise);
 }
